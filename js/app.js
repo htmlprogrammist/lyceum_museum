439577b2c7a36744e242f3427d3fb1d78bd2154f
Egor Badmaev <badmaeve2511@gmail.com>
2020-10-29 17:51:53 +0300

    feat and refactor: add new assets, refactor code

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
 function burgerMenu() {
-  let menu = document.querySelector('.burger-menu');
-  let button = menu.querySelector('.burger-menu__button');
-  let links = menu.querySelector('.burger-menu__link');
-  let overlay = menu.querySelector('.burger-menu__overlay');
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    toggleMenu();
-  });
-  links.addEventListener('click', () => toggleMenu());
-  overlay.addEventListener('click', () => toggleMenu());
-  function toggleMenu() {
-    menu.classList.toggle('burger-menu_active');
-    if (menu.classList.contains('burger-menu_active')) {
-      document.querySelector('body').style.overflow = 'hidden'
-    } else {
-      document.querySelector('body').style.overflow = 'visible'
+    let menu = document.querySelector('.burger-menu');
+    let button = menu.querySelector('.burger-menu__button');
+    let links = menu.querySelector('.burger-menu__link');
+    let overlay = menu.querySelector('.burger-menu__overlay');
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+    links.addEventListener('click', () => toggleMenu());
+    overlay.addEventListener('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.classList.toggle('burger-menu_active');
+        if (menu.classList.contains('burger-menu_active')) {
+            document.querySelector('body').style.overflow = 'hidden'
+        } else {
+            document.querySelector('body').style.overflow = 'visible'
+        }
     }
-  }
 }
 
 burgerMenu();
